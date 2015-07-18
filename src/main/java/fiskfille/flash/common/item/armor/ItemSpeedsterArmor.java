8436703fac3d98dbdd523e9983b62b2f152019da
T0psid3xDev <altisimomods@gmail.com>
2015-07-18 11:54:47 -0700

    Pull Request

--- src/main/java/fiskfille/flash/common/item/armor/ItemSpeedsterArmor.java
+++ src/main/java/fiskfille/flash/common/item/armor/ItemSpeedsterArmor.java
@@ -1,14 +1,22 @@
 package fiskfille.flash.common.item.armor;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+
+import org.lwjgl.opengl.GL11;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import fiskfille.flash.Flash;
 import fiskfille.flash.common.speedster.Speedster;
 
 public abstract class ItemSpeedsterArmor extends ItemArmor
@@ -25,6 +33,47 @@ public abstract class ItemSpeedsterArmor extends ItemArmor
     
     public abstract Speedster getSpeedster();
     
+    
+    public void renderHelmetOverlay(ItemStack stack, EntityPlayer player, ScaledResolution resolution, float partialTicks, boolean hasScreen, int mouseX, int mouseY)
+    {
+    	ScaledResolution res = new ScaledResolution(Minecraft.getMinecraft(), Minecraft.getMinecraft().displayWidth, Minecraft.getMinecraft().displayHeight);
+		int width = res.getScaledWidth();
+		int height = res.getScaledHeight();
+		
+		if (res != null)
+		{
+	        Tessellator tessellator = Tessellator.instance;
+
+			GL11.glDisable(GL11.GL_TEXTURE_2D);
+			GL11.glColor4f(0.0F, 0.0F, 0.0F, 0.6F);
+	        tessellator.startDrawingQuads();
+	        tessellator.addVertexWithUV(0.0D, (double)height, -90.0D, 0.0D, 1.0D);
+	        tessellator.addVertexWithUV((double)width, (double)height, -90.0D, 1.0D, 1.0D);
+	        tessellator.addVertexWithUV((double)width, 0.0D, -90.0D, 1.0D, 0.0D);
+	        tessellator.addVertexWithUV(0.0D, 0.0D, -90.0D, 0.0D, 0.0D);
+	        tessellator.draw();
+	        GL11.glEnable(GL11.GL_TEXTURE_2D);
+	        
+			GL11.glEnable(GL11.GL_BLEND);
+	    	GL11.glDisable(GL11.GL_DEPTH_TEST);
+	        GL11.glDepthMask(false);
+	        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+	        float f = 0.5F;
+	        GL11.glColor4f(f, f, f, 1.0F);
+	        GL11.glDisable(GL11.GL_ALPHA_TEST);
+	        Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation(Flash.modid, "textures/gui/flash.png"));
+	        tessellator.startDrawingQuads();
+	        tessellator.addVertexWithUV(0.0D, (double)height, -90.0D, 0.0D, 1.0D);
+	        tessellator.addVertexWithUV((double)width, (double)height, -90.0D, 1.0D, 1.0D);
+	        tessellator.addVertexWithUV((double)width, 0.0D, -90.0D, 1.0D, 0.0D);
+	        tessellator.addVertexWithUV(0.0D, 0.0D, -90.0D, 0.0D, 0.0D);
+	        tessellator.draw();
+	        GL11.glDepthMask(true);
+	        GL11.glEnable(GL11.GL_DEPTH_TEST);
+	        GL11.glEnable(GL11.GL_ALPHA_TEST);
+	        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);	        
+		}
+    }
     @SideOnly(Side.CLIENT)
     public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemstack, int armorSlot)
     {
